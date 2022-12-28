ce74bfd016672ef4e05787f3c8076e79262713c8
Tanner McMullen <94848716+ilovespectra@users.noreply.github.com>
2022-12-28 07:41:04 -0700

    Update main.rs

--- boperator/src/main.rs
+++ boperator/src/main.rs
@@ -1,5 +1,10 @@
-// Creates a connection to a godmother
+extern crate solana_sdk;
 
+use solana_sdk::{
+    account::{Account, KeyedAccount},
+    pubkey::Pubkey,
+    system_instruction,
+};
 use std::fs;
 use std::io::Write;
 use std::net::TcpStream;
@@ -9,6 +14,20 @@ use std::time::Duration;
 fn main() {
     // Connect to the central server
     let mut stream = TcpStream::connect("central_server_address:port").unwrap();
+
+    // Get the Boperator NFT ID
+    let boperator_nft_id = Pubkey::new_rand();
+
+    // Get the wallet address of the Solana wallet associated with the node
+    let wallet_address = Pubkey::new_rand();
+
+    // Check if the Boperator NFT is present in the wallet
+    let mut wallet_account = Account::new(0, 0, &system_instruction::create_account::id(), &[]);
+    let mut keyed_wallet_account = KeyedAccount::new(&wallet_address, false, &mut wallet_account);
+    if !keyed_wallet_account.account.is_signer(&boperator_nft_id) {
+        println!("Boperator NFT not found in wallet!");
+        return;
+    }
 
     // Start a thread to periodically send data usage updates to the central server
     thread::spawn(move || {
